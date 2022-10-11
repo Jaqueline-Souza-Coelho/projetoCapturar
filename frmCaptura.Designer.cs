16588a4ba81e8547e1c697020ef52b274d8d8f45
Jaqueline-Souza-Coelho <jaquelinedesouza.coelho@yahoo.com.br>
2022-10-10 22:51:33 -0300

    primeiro commit
    primeiro commit

---
+++ frmCaptura.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace PrintScreenCSharp
+{
+    partial class frmCaptura
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCapturar = new System.Windows.Forms.Button();
+            this.btnCapturarForm = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCapturar
+            // 
+            this.btnCapturar.Location = new System.Drawing.Point(21, 234);
+            this.btnCapturar.Name = "btnCapturar";
+            this.btnCapturar.Size = new System.Drawing.Size(113, 29);
+            this.btnCapturar.TabIndex = 0;
+            this.btnCapturar.Text = "Capturar Tela";
+            this.btnCapturar.UseVisualStyleBackColor = true;
+            this.btnCapturar.Click += new System.EventHandler(this.btnCapturar_Click);
+            // 
+            // btnCapturarForm
+            // 
+            this.btnCapturarForm.Location = new System.Drawing.Point(253, 234);
+            this.btnCapturarForm.Name = "btnCapturarForm";
+            this.btnCapturarForm.Size = new System.Drawing.Size(113, 29);
+            this.btnCapturarForm.TabIndex = 1;
+            this.btnCapturarForm.Text = "Capturar Formulário";
+            this.btnCapturarForm.UseVisualStyleBackColor = true;
+            this.btnCapturarForm.Click += new System.EventHandler(this.btnCapturarForm_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(123, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Print Screen";
+            // 
+            // frmCaptura
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(391, 284);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCapturarForm);
+            this.Controls.Add(this.btnCapturar);
+            this.Name = "frmCaptura";
+            this.Text = "Capturar Imagem";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCapturar;
+        private System.Windows.Forms.Button btnCapturarForm;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ frmCaptura.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrintScreenCSharp
+{
+    public partial class frmCaptura : Form
+    {
+        public frmCaptura()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCapturar_Click(object sender, EventArgs e)
+        {
+            var path = "C:\\PrintScreen";
+
+            var resultado = CapturarTela(path);
+
+            if (resultado != "")
+                MessageBox.Show("Tela capturada em " + resultado);
+            else
+                MessageBox.Show("Não foi possível capturar a tela");
+        }
+
+        private string CapturarTela(string path)
+        {
+            try
+            {
+                var computadorTela = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+
+                var arquivoDestino = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+
+                var arq = Graphics.FromImage(arquivoDestino);
+
+                arq.CopyFromScreen(new Point(0, 0), new Point(0, 0), computadorTela);
+
+                var nomeArquivo = path + "\\" + DateTime.Now.ToString().Replace(" ", "_").Replace("/", "").Replace(":", "") + ".png";
+                arquivoDestino.Save(nomeArquivo, System.Drawing.Imaging.ImageFormat.Png);
+
+                return nomeArquivo;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Falha: " + ex.Message);
+
+                return "";
+            }
+
+        }
+
+        private void btnCapturarForm_Click(object sender, EventArgs e)
+        {
+            var path = "C:\\PrintScreen";
+
+            var resultado = CapturarForm(path, this);
+
+            if (resultado != "")
+                MessageBox.Show("Formulário capturada em " + resultado);
+            else
+                MessageBox.Show("Não foi possível capturar o formulário");
+        }
+
+        private string CapturarForm(string path, frmCaptura form)
+        {
+            try
+            {
+                var nomeArquivo = path + "\\" + DateTime.Now.ToString().Replace(" ", "_").Replace("/", "").Replace(":", "") + ".png";
+                
+                using (var bmp = new Bitmap(form.Width,form.Height))
+                {
+                    form.DrawToBitmap(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height));
+                    bmp.Save(nomeArquivo);
+                }
+                return nomeArquivo;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Falha: " + ex.Message);
+                return "";
+            }
+
+        }
+    }
+}
